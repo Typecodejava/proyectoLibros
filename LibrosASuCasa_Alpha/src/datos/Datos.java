1dbaee0fc9477988e78eb840ff7376772244b469
admin <admin@pwc1>
2018-02-06 11:36:36 +0100

    DATOS

--- LibrosASuCasa_Alpha/src/datos/Datos.java
+++ LibrosASuCasa_Alpha/src/datos/Datos.java
@@ -36,33 +36,16 @@ public class Datos implements InterfaceDatos {
 
 	
 	public ColLibros BuscarAutor(String busqueda){
-		Statement st = null;
-		ResultSet rs = null;
-		ColLibros librosDeBusqueda = new ColLibros();
 		String query ="SELECT * FROM "+BDDNAME+".libros WHERE libros.idautores IN (SELECT autores.idautores FROM "+BDDNAME+".autores WHERE nombre LIKE '%"+busqueda+"%' or apellido LIKE '%"+busqueda+"%');";
-		try{
-			st = conectar();
-			rs = st.executeQuery(query);
-			while ( rs.next() ){
-				Libro nuevoLibro = new Libro (rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getDouble(6), rs.getInt(7));
-				librosDeBusqueda.add(nuevoLibro);
-			}
-		}
-			
-		catch (SQLException e){
-			System.out.println("SQLException");
-		}
-		return librosDeBusqueda;
+
+		return this.CrearColeccion (query);
 		
 	}
-		
-		
-	public ColLibros BuscarTitulo (String busqueda){
-		
+	
+	public ColLibros CrearColeccion (String query){
 		Statement st = null;
 		ResultSet rs = null;
 		ColLibros librosDeBusqueda = new ColLibros ();
-		String query = "SELECT idLibros, isbn, titulo, descripcion, sinopsis, cantidad, precio FROM "+BDDNAME+".libros WHERE titulo LIKE '%"+busqueda+"%';";
 		try{
 			st = conectar();
 			rs = st.executeQuery(query);
@@ -77,9 +60,16 @@ public class Datos implements InterfaceDatos {
 		catch (SQLException e){
 			System.out.println("SQLException");
 		}
-		
-		
 		return librosDeBusqueda;
+	}
+		
+	public ColLibros BuscarTitulo (String busqueda){
+		
+		
+		String query = "SELECT idLibros, isbn, titulo, descripcion, sinopsis, cantidad, precio FROM "+BDDNAME+".libros WHERE titulo LIKE '%"+busqueda+"%';";
+		
+		
+		return this.CrearColeccion (query);
 		
 	}
 
